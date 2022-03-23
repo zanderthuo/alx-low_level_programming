e5936b8aec220778573c33f0615ee3387216d259
thuozander <thuozander@gmail.com>
2022-03-23 12:21:35 -0700

    Added 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,22 @@
 #include "main.h"
 /**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
+ * *_strncpy - find the length of a string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
 */
 
-char *_strncat(char *dest, char *src, int n)
-{
 
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-;
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
 
 for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
+	dest[i] = src[i];
 
-/*should end with a end of string char*/
-dest[dest_len + i] = '\0';
+for ( ; i < n; i++)
+	dest[i] = '\0';
 
 return (dest);
 }
--- 0x06-pointers_arrays_strings/README.md
+++ 0x06-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 ###0x06. C - More pointers, arrays and strings
 0. Function that concatenates two strings
 1. function that concatenates two strings.
+2. function that copies a string.
