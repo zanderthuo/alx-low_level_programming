871f5b9c34a42129da009b91dcccecbb0e253529
thuozander <thuozander@gmail.com>
2022-04-05 14:28:04 -0700

    fixed

--- 0x09-static_libraries/lib/0-isupper.c
+++ 0x09-static_libraries/lib/0-isupper.c
@@ -1,6 +1,13 @@
 #include "main.h"
 
-int _isupper(int c __attribute__((unused)))
+/**
+ * _isupper - check for upper case letter
+ * @c : character to check the case
+ * Return:0 or 1
+ */
+
+int _isupper(int c)
 {
-	return (0);
+	return (c >= 65 && c <= 90);
+
 }
